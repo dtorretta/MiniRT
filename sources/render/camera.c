a5615b29a03fa2b4fa4ee22539c6d9aed18fd648
dtorretta <danielatorretta18@gmail.com>
2024-11-25 21:36:16 +0100

    ready to start debugging

--- sources/render/camera.c
+++ sources/render/camera.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   camera.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miguandr <miguandr@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 09:02:24 by miguandr          #+#    #+#             */
-/*   Updated: 2024/11/18 14:09:50 by miguandr         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:34:00 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ void	init_camera(t_camera *camera)
 	// Normalize camera direction and convert FOV to radians
 	camera->aim = ft_normalize(&camera->aim);
 	camera->focal_length = 1.0f;
-	camera->fov *= M_PI / 180.0f;
+	camera->fov *= 3.14159265358979323846 / 180.0f;
 	calculate_camera_basis(camera);
 	//viewport dimensions
 	aspect_ratio = (float)HEIGHT / (float)WIDTH;
